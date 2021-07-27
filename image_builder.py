6904ce822f6e9671a8d943d2fcc492de94010bde
Scott Ernst <swernst@gmail.com>
2021-07-27 14:01:51 -0500

    Tagged CI Releases
    Add tag release support to the CI pipelines.

--- image_builder.py
+++ image_builder.py
@@ -3,10 +3,11 @@ import argparse
 import os
 import pathlib
 import textwrap
+import typing
 
 import toml
 
-HUB_PREFIX = "swernst/reviser"
+HUB_PREFIXES = ["docker.io/swernst/reviser", "docker.io/rocketboosters/reviser"]
 MY_DIRECTORY = pathlib.Path(__file__).parent.absolute()
 PROJECT_DATA = toml.loads(MY_DIRECTORY.joinpath("pyproject.toml").read_text())
 VERSION = PROJECT_DATA["tool"]["poetry"]["version"]
@@ -15,23 +16,43 @@ BUILDS = {
 }
 
 
-def build(python_version: str, spec: dict, args: argparse.Namespace) -> dict:
-    """Build the container from the specified docker file path."""
-    path = MY_DIRECTORY.joinpath("Dockerfile")
+def _append_to_script(script_path: pathlib.Path, *args: str):
+    """Append the lines of args to the script path file."""
+    print("[SCRIPT]: Writing command to script.")
+    with open(script_path, mode="a") as f:
+        f.write("\n".join(args).replace(" --", " \\\n   --"))
+        f.write("\n")
 
+
+def _get_tags(args: argparse.Namespace, python_version: str) -> typing.List[str]:
+    """Create a list of tags to apply to the built image."""
     version = "{}{}".format(
         "pre-" if args.pre else "",
         VERSION,
     )
     generic = "pre" if args.pre else "latest"
 
-    tags = [
-        "{}:{}-{}".format(HUB_PREFIX, version, python_version),
-        "{}:{}-{}".format(HUB_PREFIX, generic, python_version),
-    ]
-    if not args.pre:
-        tags.append("{}:current-{}".format(HUB_PREFIX, python_version))
+    tags: typing.List[str] = []
+    for prefix in HUB_PREFIXES:
+        tags += [
+            "{}:{}-{}".format(prefix, version, python_version),
+            "{}:{}-{}".format(prefix, generic, python_version),
+        ]
+        if not args.pre:
+            tags.append("{}:current-{}".format(prefix, python_version))
 
+    return tags
+
+
+def build(
+    python_version: str,
+    spec: dict,
+    args: argparse.Namespace,
+    script_path: pathlib.Path = None,
+) -> dict:
+    """Build the container from the specified docker file path."""
+    path = "./Dockerfile"
+    tags = _get_tags(args, python_version)
     parts = ["docker", "build", "--pull", f'--file="{path}"']
     if not args.cache:
         parts.append("--no-cache")
@@ -45,9 +66,12 @@ def build(python_version: str, spec: dict, args: argparse.Namespace) -> dict:
     command = " ".join(parts + ["."])
 
     print("[BUILDING]:", python_version)
+    print(textwrap.indent(command.replace(" -", "\n   -"), "   "))
+
     if args.dry_run:
         print("[DRY-RUN]: Skipped building command")
-        print(textwrap.indent(command.replace(" -", "\n   -"), "   "))
+    elif script_path:
+        _append_to_script(script_path, command)
     else:
         os.environ["DOCKER_BUILDKIT"] = "1"
         os.system(command)
@@ -55,14 +79,23 @@ def build(python_version: str, spec: dict, args: argparse.Namespace) -> dict:
     return dict(spec=spec, id=python_version, path=path, command=command, tags=tags)
 
 
-def publish(build_entry: dict, args: argparse.Namespace):
+def publish(
+    build_entry: dict,
+    args: argparse.Namespace,
+    script_path: pathlib.Path = None,
+):
     """Publish the specified build entry to docker hub."""
     for tag in build_entry["tags"]:
+        command = "docker push {}".format(tag)
+        print(f"[PUSH]: Pushing tag {tag}")
+        print(textwrap.indent(command.replace(" -", "\n   -"), "   "))
+
         if args.dry_run:
             print("[DRY-RUN]: Skipped pushing {}".format(tag))
+        elif script_path:
+            _append_to_script(script_path, command)
         else:
-            print("[PUSHING]:", tag)
-            os.system("docker push {}".format(tag))
+            os.system(command)
 
 
 def parse() -> argparse.Namespace:
@@ -71,6 +104,7 @@ def parse() -> argparse.Namespace:
     parser.add_argument(
         "-p",
         "--publish",
+        "--push",
         action="store_true",
         help="Whether or not to publish images after building them.",
     )
@@ -115,15 +149,37 @@ def parse() -> argparse.Namespace:
             """,
         ),
     )
+    parser.add_argument(
+        "--script-path",
+        help=textwrap.dedent(
+            """
+            When specified, the commands will be written to a script instead of
+            executed. This is used for multi-stage CI where Python execution and
+            docker commands cannot be easily colocated.
+            """
+        ),
+    )
     return parser.parse_args()
+
+
+def _initialize_script_path(args: argparse.Namespace) -> typing.Optional[pathlib.Path]:
+    """Initialize the output script if it exists."""
+    if not args.script_path:
+        return None
+
+    p = pathlib.Path(args.script_path).resolve()
+    p.write_text('#!/bin/sh\nexport DOCKER_BUILDKIT="1"\n')
+    p.chmod(0o775)
+    return p
 
 
 def run():
     """Execute the build process."""
     args = parse()
+    script_path = _initialize_script_path(args)
 
     build_results = [
-        build(python_version, spec, args)
+        build(python_version, spec, args, script_path)
         for python_version, spec in BUILDS.items()
         if not args.ids or python_version in args.ids
     ]
@@ -132,7 +188,7 @@ def run():
         return
 
     for entry in build_results:
-        publish(entry, args)
+        publish(entry, args, script_path)
 
 
 if __name__ == "__main__":
