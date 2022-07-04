c227b4bfee98551f27431e7a7e19a7eb9b3bbec6
Kevin Schiroo <kjschiroo@gmail.com>
2022-07-04 12:21:09 -0500

    Add local bundle output
    This MR adds in the ability to access a bundled artifact locally. I'm
    experimenting with using terraform as the deployment method on a
    personal project and the ability to easily get at the bundle would be
    handy.

--- reviser/commands/bundler.py
+++ reviser/commands/bundler.py
@@ -5,9 +5,12 @@ The resulting zip file is structured correctly to be deployed to the lambda
 function/layer target via an S3 upload and subsequent publish command.
 """
 import argparse
+import pathlib
+import shutil
 import typing
 
 from reviser import bundling
+from reviser import definitions
 from reviser import interactivity
 
 
@@ -31,6 +34,27 @@ def populate_subparser(parser: argparse.ArgumentParser):
             installed even if they had been installed previously.
             """,
     )
+    parser.add_argument(
+        "--output",
+        "-o",
+        help="Output the bundled artifacts into the specified output path.",
+    )
+
+
+def _copy_to_output(
+    targets: typing.List["definitions.Target"], output_dir: typing.Optional[str]
+):
+    """Copy the outputs of the targets into the specified output directory."""
+    if not output_dir:
+        return
+    output_path = pathlib.Path(output_dir)
+    output_path.mkdir(parents=True, exist_ok=True)
+    for target in targets:
+        for name in target.names:
+            shutil.copy(
+                target.bundle_zip_path,
+                output_path.joinpath(f"{name}-{target.kind.value}.zip"),
+            )
 
 
 def run(ex: "interactivity.Execution"):
@@ -40,6 +64,7 @@ def run(ex: "interactivity.Execution"):
         selection=ex.shell.selection,
         reinstall=ex.args.get("reinstall", False),
     )
+    _copy_to_output(bundled_targets.targets, ex.args.get("output"))
     print("\n\n")
     return ex.finalize(
         status="BUNDLED",
